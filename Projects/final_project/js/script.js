29a91e7c72e0708367667d6754fc73ceabd28965
Tardifg <gtardif@eastsideprep.org>
2023-03-01 13:14:34 -0800

    Paralax.js

--- Projects/final_project/js/script.js
+++ Projects/final_project/js/script.js
@@ -1,19 +1,3 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var element = document.getElementById("nav");
-var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.display = "none";
-     /*element.classList.add("outfade");
-     element.classList.remove("infade");*/
- } else {
-     document.getElementById("nav").style.display = "inline-block";
-     /*element.classList.add("infade");
-     element.classList.remove("outfade");*/
-}
-prevScrollpos = currentScrollPos;
-}
-
 
 window.addEventListener(
     "scroll",
@@ -26,15 +10,13 @@ window.addEventListener(
     false
   );
   
-
-
   window.onscroll = function() {
-    var element = document.getElementById("footer");
+    var element = document.getElementById("galfooter");
     var scroll = --scroll
     if (--scroll >  0.99) {
          element.classList.add("animate__animated animate__fadeInUp");
-         document.getElementById("footer").style.display = "block";
+         document.getElementById("galfooter").style.display = "block";
      } else {
-         document.getElementById("footer").style.display = "block";
+         document.getElementById("galfooter").style.display = "block";
     }
     }
